2d2d7138340b02d6d0056d5d9d33d9bbbb44c0ed
李婉 <iwan16406783@gmail.com>
2017-03-20 20:04:59 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,5 +1,5 @@
 class Account::PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
     @posts = current_user.posts
   end
